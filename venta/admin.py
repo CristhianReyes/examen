786fd1d8a7ab2a72eba9485249c0793a96d422e1
Cristhian <cristhian-9292@hotmail.com>
2016-11-22 18:12:07 -0600

    Creacion de tablas en Django

--- venta/admin.py
+++ venta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Productos
+from .models import Usuarios
+from .models import Venta
 
-# Register your models here.
+admin.site.register(Productos)
+admin.site.register(Usuarios)
+admin.site.register(Venta)
